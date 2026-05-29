12bd2bc2350f93ce39d16af033c4f7e90ce662a8
yasmink111 <yasminkassem098@gmail.com>
2026-05-29 04:07:29 +0300

    Fix routing (remove BrowserRouter, use HashRouter)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import SkinCare from "./pages/SkinCare";
--- src/index.js
+++ src/index.js
@@ -3,19 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-
-import {
-    BrowserRouter
-} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(
     document.getElementById("root")
 ).render(
 
-    <BrowserRouter>
+    <HashRouter>
 
         <App />
 
-    </BrowserRouter>
+    </HashRouter>
 
 );
